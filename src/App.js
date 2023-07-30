2913b35172c1be97e1b873bd97bb3e1a8faaeef9
davidwills3045 <davidonaijaflavour612@gmail.com>
2023-07-30 20:10:51 +0100

    An-exam-portal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
+import Display from "./components/Display";
+import Examinationpage from './components/Examinationpage';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Donepage from './components/Donepage';
+import Instructionpage from './components/Instructionpage';
 
 function App() {
+  const [Questions, setQuestions] = useState(null)
+  const [ShuffledQuestion, setShuffledQuestion] = useState(null)
+  const [subject, setSubject] = useState(null)
+  const [Password, setPassword] = useState("");
+  const [Username, setUsername] = useState("");
+  const [TermAccessible, setTermAccessible] = useState(null);
+  const [studentData, setStudentData] = useState(null);
+  const [TotalMinutes, setTotalMinutes] = useState(0)
+  const [Shuffle, setShuffle] = useState(true)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+        <Route path='/' element={<Display
+          setTotalMinutes={setTotalMinutes}
+            Questions={Questions}
+            setQuestions={setQuestions}
+            setSubject={setSubject}
+            setPassword={setPassword}
+            Password={Password}
+            setShuffle={setShuffle}
+            Shuffle={Shuffle}
+            setUsername={setUsername}
+            Username={Username}
+            TotalMinutes={TotalMinutes}
+            studentData ={studentData}
+             setStudentData = {setStudentData}
+            TermAccessible={TermAccessible}
+            setTermAccessible={setTermAccessible}
+            subject={subject} />} />
+         <Route path='/Examinationpage' element={<Examinationpage
+            Questions={Questions}
+            setSubject={setSubject}
+            subject={subject}
+            setShuffle={setShuffle}
+            Shuffle={Shuffle}
+            TotalMinutes={TotalMinutes}
+            setPassword={setPassword}
+            Password={Password}
+            setUsername={setUsername}
+            studentData ={studentData}
+             setStudentData = {setStudentData}
+            TermAccessible={TermAccessible}
+            setTermAccessible={setTermAccessible}
+            Username={Username} />} />
+          <Route path='/Donepage' element={<Donepage />} />
+          <Route path='/instructionpage' element={<Instructionpage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Instructionpage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Instructionpage = () => {
+  return (
+    <>
+      <div className="instruction-div">
+        <div className="instruct-div1">
+          <h1>INSTRUCTIONS FOR THIS TERM EXAMINATION</h1>
+        </div>
+        <div className="instruct-div2">
+          <div className="instruct-div3">
+            <div>
+              <p>
+                <span>1.)</span> The allocated time for this examination is{" "}
+                <span>___</span> and you will be logged out on its the
+                expiration.
+              </p>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div>
+              <p>
+                <span>2.)</span> You are required to comply with the directions
+                given by the head invigilator at the examination venue.
+              </p>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div>
+              <p>
+                <span>3.)</span> You cannot withdraw from the examination once
+                you have clicked the START button.
+              </p>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div>
+              <p>
+                <span>4.)</span> Ensure that you have submitted your examination
+                paper electronically before you leave the venue.
+              </p>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div>
+              <span>5.)</span> Examination malpractice is highly prohibited.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Instructionpage;
